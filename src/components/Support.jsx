a0813e0119715827aba17e7c8e66c648aab3112f
fgholizade <gholizadefatemeh37@gmail.com>
2023-04-03 18:10:33 +0330

    Add map function for cards

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 
 import { PhoneIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import {CpuChipIcon, Cog6ToothIcon} from '@heroicons/react/24/solid'
-
 import supportImg from '../assets/support.png'
 
 const Support = () => {
+const [data,setData] = useState([])
+    async function fetchJSON(request) {
+        try {
+          const response = await fetch(request);
+          const contentType = response.headers.get("content-type");
+          if (!contentType || !contentType.includes("application/json")) {
+            throw new TypeError("Oops, we haven't got JSON!");
+          }
+          const jsonData = await response.json();
+          setData(jsonData)
+          console.log(data)
+
+          
+        } catch (error) {
+          console.error("Error:", error);
+        }
+      }
+      useEffect(()=>{
+        fetchJSON("https://mocki.io/v1/d4867d8b-b5d5-4a48-a4ab-79131b5809b8")
+      },[])
+    
   return (
   <div name='support' className='w-full mt-24'>
       <div className='w-full h-[700px] bg-gray-900/90 absolute'>
@@ -19,18 +39,20 @@ const Support = () => {
               <p className='py-4 text-3xl text-slate-300'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In repudiandae veritatis ratione error tenetur, voluptates architecto possimus ad! Omnis minima ea quidem quisquam unde beatae, minus illo et cum vel?</p>
           </div>
 
-          <div className='grid grid-cols-1 lg:grid-cols-3 relative gap-x-8 gap-y-16 px-4 pt-12 sm:pt-20 text-black'>
-              <div className='bg-white rounded-xl shadow-2xl'>
+          <div className='grid grid-cols-1 lg:grid-cols-4 relative gap-x-8 gap-y-16 px-4 pt-12 sm:pt-20 text-black'>
+          {data.map((user) => (
+            <div className='bg-white rounded-xl shadow-2xl'>
                   <div className='p-8'>
                       <PhoneIcon className='w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]' />
-                      <h3 className='font-bold text-2xl my-6'>Sales</h3>
-                      <p className='text-gray-600 text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi provident iure placeat blanditiis ea sint earum hic iste quibusdam exercitationem.</p>
+                      <h3 className='font-bold text-2xl my-6'>{user.name}</h3>
+                      <p className='text-gray-600 text-xl'>Lives in: {user.city}</p>
                   </div>
                   <div className='bg-slate-100 pl-8 py-4'>
-                      <p className='flex items-center text-indigo-600'>Contact Us <ArrowRightIcon className='w-5 ml-2' /></p>
+                      <p className='flex items-center text-indigo-600'>Contact him/her <ArrowRightIcon className='w-5 ml-2' /></p>
                   </div>
-              </div>
-              <div className='bg-white rounded-xl shadow-2xl'>
+              </div>      ))}
+              
+              {/* <div className='bg-white rounded-xl shadow-2xl'>
                   <div className='p-8'>
                       <Cog6ToothIcon className='w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]' />
                       <h3 className='font-bold text-2xl my-6'>Technical Support</h3>
@@ -49,7 +71,7 @@ const Support = () => {
                   <div className='bg-slate-100 pl-8 py-4'>
                       <p className='flex items-center text-indigo-600'>Contact Us <ArrowRightIcon className='w-5 ml-2' /></p>
                   </div>
-              </div>
+              </div> */}
           </div>
       </div>
   </div>
